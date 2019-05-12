95422f687c3edd51c92fe46b90cf1d2596e89ab4
施懿民 <shiyimin@vowei.com>
2019-05-12 15:46:07 +0800

    签入准备发币的版本

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,5 @@
 const SwapCoin = artifacts.require("Swap");
-const SwapStakeHolders = artifacts.require("SwapStakeHolders");
-const BatchAddStakeHolders = artifacts.require("BatchAddStakeHolders");
 
 module.exports = function(deployer) {
   deployer.deploy(SwapCoin);
-  deployer.deploy(SwapStakeHolders);
-  deployer.deploy(BatchAddStakeHolders);
 };
--- test/checkownship.js
+++ test/checkownship.js
@@ -1,62 +1,10 @@
 const SwapCoin = artifacts.require("Swap");
-const BatchAddStakeHolders = artifacts.require("BatchAddStakeHolders");
-const SwapStakeHolders = artifacts.require("SwapStakeHolders");
-const HolderLockStrategy = artifacts.require("HolderLockStrategy");
 
 contract('CheckOwnerShip', (accounts) => {
   it('should forbid no-owner do owner operation', async () => {
-    const bash = await BatchAddStakeHolders.new();
-    const stakes = await SwapStakeHolders.new();
-
-    var owner = accounts[0];
-    // investors
-    // 0xD9018C7EF5020CD200d1ff508391fA2A440280Bc
-    const accountOne = accounts[1];
-    // 0xfC6d6e78A016b27d2D4E903a118cE8562F99f73F
-    const accountTwo = accounts[2];
-    var response = await bash.investors(owner);
-    var investors = await HolderLockStrategy.at(response.logs[0].args.addr);
-    response = await bash.teamMembers(owner);
-    var teams = await HolderLockStrategy.at(response.logs[0].args.addr);
-    response = await bash.funds(owner);
-    var funds = await HolderLockStrategy.at(response.logs[0].args.addr);    
+    var owner = accounts[0]; 
+    var accountOne = accounts[1];
     const SwapCoinInstance = await SwapCoin.new();
-
-    // 2020-5-3 8:00:00 AM，
-    var today = new Date(2020, 4, 3, 8, 0, 0).getTime() / 1000;
-    // 非owner无法设置锁仓代码要求的时间
-    try {
-        await investors.setToday(today, {from: accountTwo});
-        assert.fail("非owner不应该可以设置锁仓的时间！");
-    } catch (e) {}
-    try {
-        await teams.setToday(today, {from: accountTwo});
-        assert.fail("非owner不应该可以锁仓的时间！");
-    } catch (e) {}
-    try {
-        await funds.setToday(today, {from: accountTwo});
-        assert.fail("非owner不应该可以锁仓的时间！");
-    } catch (e) {}
-
-    // 非owner无法设置stakehold
-    try {
-        await stakes.addLockStrategy(investors.address, {from: accountTwo});
-        assert.fail("非owner不应该可以添加stakehold！");
-    } catch (e) {}
-    try {
-        await stakes.addLockStrategy(teams.address, {from: accountTwo});
-        assert.fail("非owner不应该可以添加stakehold！");
-    } catch (e) {}
-    try {
-        await stakes.addLockStrategy(funds.address, {from: accountTwo});
-        assert.fail("非owner不应该可以添加stakehold！");
-    } catch (e) {}
-
-    // 非owner不可以设置锁仓规则
-    try {
-        await SwapCoinInstance.setLockStrategy(stakes.address, {from: accountTwo});
-        assert.fail("非owner不应该可以设置锁仓规则！");
-    } catch (e) {}
 
     // 非owner不可以增发
     var balanceBefore = (await SwapCoinInstance.balanceOf.call(accountOne)).toNumber();
@@ -85,43 +33,9 @@ contract('CheckOwnerShip', (accounts) => {
   });
 
   it('should allow owner do owner operation', async () => {
-    const bash = await BatchAddStakeHolders.new();
-    const stakes = await SwapStakeHolders.new();
-
     var owner = accounts[0];
-    // investors
-    // 0xD9018C7EF5020CD200d1ff508391fA2A440280Bc
-    const accountOne = accounts[1];
-    // 0xfC6d6e78A016b27d2D4E903a118cE8562F99f73F
-    const accountTwo = accounts[2];
-    var response = await bash.investors(owner);
-    var investors = await HolderLockStrategy.at(response.logs[0].args.addr);
-    response = await bash.teamMembers(owner);
-    var teams = await HolderLockStrategy.at(response.logs[0].args.addr);
-    response = await bash.funds(owner);
-    var funds = await HolderLockStrategy.at(response.logs[0].args.addr);    
+    var accountOne = accounts[1];
     const SwapCoinInstance = await SwapCoin.new();
-
-    // 2020-5-3 8:00:00 AM，
-    var today = new Date(2020, 4, 3, 8, 0, 0).getTime() / 1000;
-    // owner可以设置锁仓代码要求的时间
-    await investors.setToday(today, {from: owner});
-    var actual = await investors.getDate();
-    assert.equal(today, actual, "owner无法设置锁仓的时间！");
-    await teams.setToday(today, {from: owner});
-    actual = await teams.getDate();
-    assert.equal(today, actual, "owner无法设置锁仓的时间！");
-    await funds.setToday(today, {from: owner});
-    actual = await funds.getDate();
-    assert.equal(today, actual, "owner无法设置锁仓的时间！");
-
-    // owner可以设置stakehold
-    await stakes.addLockStrategy(investors.address, {from: owner});
-    await stakes.addLockStrategy(teams.address, {from: owner});
-    await stakes.addLockStrategy(funds.address, {from: owner});
-
-    // owner可以设置锁仓规则
-    await SwapCoinInstance.setLockStrategy(stakes.address, {from: owner});
 
     // owner可以增发
     var balanceBefore = (await SwapCoinInstance.balanceOf.call(accountOne)).toNumber();
